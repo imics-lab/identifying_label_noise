cd5c2a1f9d94c06cc3d38b31280b90364033d603
Gentry Atkinson <gma23@txstate.edu>
2020-05-13 16:27:27 -0500

    trying labelfix on some synthetic time series data

---
+++ src/find_in_synthetic_timeSeries.py
@@ -0,0 +1,6 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 13 May, 2020
+#This code will generate a numpy array of synthetic time series data, use the
+#labelfix preprocessor to flip mu percent (0.03) of the labels, and the use the
+#labelfix check_dataset function to identify some of the flipped labelsself.
