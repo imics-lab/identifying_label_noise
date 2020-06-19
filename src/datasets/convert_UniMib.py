43fe2f20235d2c8810d0c5a2c86ec6f66333a20a
Gentry Atkinson <gma23@txstate.edu>
2020-06-19 12:02:34 -0500

    adding unimib converter

---
+++ src/datasets/convert_UniMib.py
@@ -0,0 +1,7 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 17 June, 2020
+#This code will read the UniMib mat files into CSV and rename them to fit the
+#conventions being used in the 3 test files
+
+from scipy.io import loadmat
