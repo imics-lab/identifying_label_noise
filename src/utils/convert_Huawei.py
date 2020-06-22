082877c70e30313bd0d348d973e9199f2565d29f
Gentry Atkinson <gma23@txstate.edu>
2020-06-22 16:15:34 -0500

    adding huawei converter

---
+++ src/utils/convert_Huawei.py
@@ -0,0 +1,88 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 17 June, 2020
+#This code will read the UniMib mat files into CSV and rename them to fit the
+#conventions being used in the 3 test files
+
+import numpy as np
+import random
+import gc
+from scipy.io import loadmat
+from ts_feature_toolkit import get_features_for_set
+
+
+NUM_LINES = 196072
+SAMPLE_SIZE = 500
+
+if __name__ == "__main__":
+    # x_filename = "src/datasets/Huawei/Acc_x.txt"
+    # y_filename = "src/datasets/Huawei/Acc_y.txt"
+    # z_filename = "src/datasets/Huawei/Acc_z.txt"
+    #
+    label_file = "src/datasets/Huawei/Acc_labels.csv"
+    #
+    # x_file = open(x_filename, 'r')
+    # y_file = open(y_filename, 'r')
+    # z_file = open(z_filename, 'r')
+    #
+    # data = np.zeros((NUM_LINES, SAMPLE_SIZE))
+    # line = 0
+    #
+    # while(True):
+    #     x_line = x_file.readline()
+    #     y_line = y_file.readline()
+    #     z_line = z_file.readline()
+    #     if (x_line=='' or y_line=='' or z_line==''):
+    #         break
+    #
+    #     x_array = x_line.split(" ")
+    #     y_array = y_line.split(" ")
+    #     z_array = z_line.split(" ")
+    #
+    #     for j in range(SAMPLE_SIZE):
+    #         data[line][j] = np.linalg.norm([x_array[j], y_array[j], z_array[j]])
+    #
+    #
+    #     if line % 1000 == 0:
+    #         print(line, ": ", data[line][0])
+    #     line += 1
+    #
+    #
+    # print("Data array filled")
+    #
+    # features = get_features_for_set(data, num_samples=len(data))
+    # gc.collect()
+    #
+    # print("Feature array filled")
+    #
+    # np.savetxt("src/datasets/huawei1_data.csv", data, delimiter=",")
+    # np.savetxt("src/datasets/huawei1_features.csv", features, delimiter=",")
+    #
+    # print("Data and Features written to CSV")
+    # data = 0
+    # features = 0
+    # gc.collect()
+
+    print("Adding label noise to label array")
+
+    labels_array = np.genfromtxt(label_file, delimiter=' ', dtype='int')
+    labels = np.zeros(len(labels_array), dtype='int')
+    labels_with_noise = np.zeros(len(labels_array), dtype='int')
+    gremlinCounter = 0
+    NUM_CLASSES = max(labels)
+    badIndexes = np.array([], dtype='int')
+    for i in range(len(labels)):
+        labels[i] = np.floor(np.mean(labels_array[i]))-1
+        gremlin = random.randint(0,100)
+        if gremlin < 3:
+            labels[i] = (labels[i]+1)%NUM_CLASSES
+            badIndexes = np.append(badIndexes, [i])
+            gremlinCounter += 1
+        else:
+            labels_with_noise[i] = labels[i]
+
+    np.savetxt("src/datasets/huawei_with_noise1_labels.csv", labels, delimiter=",", fmt="%d")
+    np.savetxt("src/datasets/huawei_with_noise1_indexes.csv", badIndexes, delimiter=",", fmt="%d")
+
+    print(gremlinCounter, " bad labels")
+    print("All done")
--- src/utils/convert_UniMib.py
+++ src/utils/convert_UniMib.py
@@ -11,29 +11,30 @@ from ts_feature_toolkit import get_features_for_set
 
 
 if __name__ == "__main__":
-    # twoClass_X = loadmat('src/datasets/UniMiB-SHAR/data/two_classes_data.mat')['two_classes_data']
-    # twoClass_y = loadmat('src/datasets/UniMiB-SHAR/data/two_classes_labels.mat')['two_classes_labels'][:,0]
-    # #twoClass_feat = get_features_for_set(twoClass_X, num_samples=len(twoClass_X))
-    #
-    # twoClass_y = twoClass_y - 1
-    #
-    # np.savetxt("src/datasets/unimib1_data.csv", twoClass_X, delimiter=",")
-    # np.savetxt("src/datasets/unimib1_labels.csv", twoClass_y, delimiter=",", fmt="%d")
-    # #np.savetxt("src/datasets/unimib1_features.csv", twoClass_feat, delimiter=",")
-    #
-    # gremlinCounter = 0
-    # badIndexes = np.array([], dtype='int')
-    # for i in range(len(twoClass_y)):
-    #     gremlin = random.randint(0,100)
-    #     if gremlin < 3:
-    #         twoClass_y[i] = (twoClass_y[i]+1)%2
-    #         badIndexes = np.append(badIndexes, [i])
-    # print (gremlinCounter, " bad indexes in two class data")
-    #
-    # np.savetxt("src/datasets/unimib_with_noise1_labels.csv", twoClass_y, delimiter=",", fmt="%d")
-    # #np.savetxt("src/datasets/unimib_with_noise1_features.csv", twoClass_feat, delimiter=",")
-    # np.savetxt("src/datasets/unimib_with_noise1_data.csv", twoClass_X, delimiter=",")
-    # np.savetxt("src/datasets/unimib_with_noise1_indexes.csv", badIndexes, delimiter=",", fmt="%d")
+    twoClass_X = loadmat('src/datasets/UniMiB-SHAR/data/two_classes_data.mat')['two_classes_data']
+    twoClass_y = loadmat('src/datasets/UniMiB-SHAR/data/two_classes_labels.mat')['two_classes_labels'][:,0]
+    #twoClass_feat = get_features_for_set(twoClass_X, num_samples=len(twoClass_X))
+
+    twoClass_y = twoClass_y - 1
+
+    np.savetxt("src/datasets/unimib1_data.csv", twoClass_X, delimiter=",")
+    np.savetxt("src/datasets/unimib1_labels.csv", twoClass_y, delimiter=",", fmt="%d")
+    #np.savetxt("src/datasets/unimib1_features.csv", twoClass_feat, delimiter=",")
+
+    gremlinCounter = 0
+    badIndexes = np.array([], dtype='int')
+    for i in range(len(twoClass_y)):
+        gremlin = random.randint(0,100)
+        if gremlin < 3:
+            twoClass_y[i] = (twoClass_y[i]+1)%2
+            badIndexes = np.append(badIndexes, [i])
+            gremlinCounter += 1
+    print (gremlinCounter, " bad indexes in two class data")
+
+    np.savetxt("src/datasets/unimib_with_noise1_labels.csv", twoClass_y, delimiter=",", fmt="%d")
+    #np.savetxt("src/datasets/unimib_with_noise1_features.csv", twoClass_feat, delimiter=",")
+    np.savetxt("src/datasets/unimib_with_noise1_data.csv", twoClass_X, delimiter=",")
+    np.savetxt("src/datasets/unimib_with_noise1_indexes.csv", badIndexes, delimiter=",", fmt="%d")
 
     allClass_X = loadmat('src/datasets/UniMiB-SHAR/data/acc_data.mat')['acc_data']
     allClass_y = loadmat('src/datasets/UniMiB-SHAR/data/acc_labels.mat')['acc_labels'][:,0]
@@ -52,6 +53,7 @@ if __name__ == "__main__":
         if gremlin < 3:
             allClass_y[i] = (allClass_y[i]+1)%2
             badIndexes = np.append(badIndexes, [i])
+            gremlinCounter += 1
     print (gremlinCounter, " bad indexes in all class data")
     np.savetxt("src/datasets/unimib_with_noise2_data.csv", allClass_X, delimiter=",")
     np.savetxt("src/datasets/unimib_with_noise2_labels.csv", allClass_y, delimiter=",", fmt="%d")
