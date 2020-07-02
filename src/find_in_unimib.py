c061e22cd528e0390614efcc12ddcf008c10c78c
Gentry Atkinson <gma23@txstate.edu>
2020-07-01 20:50:11 -0500

    starting script to find bad unimib indexes

---
+++ src/find_in_unimib.py
@@ -0,0 +1,7 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 01 July, 2020
+#This file will identify the 10 most suspect indexes in the UniMib SHAR dataset
+#and will write a visualization of those points to file
+
+if __name__ == "__main__":
